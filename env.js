8cf6d6cdf5162cc06736c5dbb39685f7aec7586f
michaelclevenger <clevengermichael93@yahoo.com>
2021-09-16 16:37:36 -0500

    env-js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-    host: "localhosthttps://capstone-project-real-main.herokuapp.com/",
+    host: "https://capstone-project-real-main.herokuapp.com/",
     port: 8080,
   },
 };
