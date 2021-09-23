267a05d6e4985251d89fc2147b7e5c58a4c5f0fb
michaelclevenger <clevengermichael93@yahoo.com>
2021-09-23 17:55:09 -0500

    'commit'

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -5,9 +5,6 @@ import "./hero-section.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <div
-        styles={{ backgroundImage: `url("https://i.imgur.com/uJF2tz3.gif")` }}
-      />
       <p>
         My son is the best thing in my life, and I wanted to do a project that
         reminded me of why I'm doing all this.
