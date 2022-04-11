4b34bc5608028b8bc2e9079c2396c29609fefca3
michaelclevenger <clevengermichael93@yahoo.com>
2022-04-11 17:18:30 -0500

    commit

--- src/components/cards.js
+++ src/components/cards.js
@@ -12,37 +12,37 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="https://i.imgur.com/CACzmqD.jpg"
-              text="Eddy's First Experiences"
+              text="Eddy's First Experiences!"
               label="Year Zero"
               path="/zero"
             />
             <CardItem
               src="https://i.imgur.com/Vp996Pi.jpg"
-              text="He Grew So Fast"
+              text="He Grew So Fast!"
               label="Year One"
               path="/one"
             />
             <CardItem
               src="https://i.imgur.com/Z8fXMA8.jpg"
-              text="The Terrible Two's"
+              text="The Terrible Two's!"
               label="Year Two"
               path="/two"
             />
             <CardItem
               src="https://i.imgur.com/I1ZK2RC.jpg"
-              text="Growth Spurt Madness"
+              text="Growth Spurt Madness!"
               label="Year Three"
               path="/three"
             />
             <CardItem
               src="https://i.imgur.com/Kmcyr3R.jpg"
-              text="My Little Boy Is Now Huge"
+              text="My Little Boy Is Now Huge!"
               label="Year Four"
               path="/four"
             />
             <CardItem
               src="https://imgur.com/203OsM5"
-              text="It's Official, He's A 'Big Boy' Now"
+              text="It's Official, He's A 'Big Boy' Now!"
               label="Year Five"
               path="/five"
             />
